d9b1a60f22a2ea1b68d0f70526b1624778ad5b6a
Ryan Denney <rrdenney@bsu.edu>
2019-04-05 11:50:00 -0400

    ExtractionMethods

--- src/ifs/Refactoring.java
+++ src/ifs/Refactoring.java
@@ -7,12 +7,24 @@ public class Refactoring {
             return income*0.365;
         } else if (income <= 30000) {
             return getaDouble(income, 10000, 0.2, 35600);
-        } else if (income <= 60000) {
+        } else if (income <= getAdjustment()) {
             return getaDouble(income, 30000, 0.1, 76500);
         } else {
-            return getaDouble(income, 60000, 0.02, 105600);
+            return getaDouble(income, getAdjustment(), getWeight(), getConstant());
         }
 
+    }
+
+    private int getConstant() {
+        return 105600;
+    }
+
+    private double getWeight() {
+        return 0.02;
+    }
+
+    private int getAdjustment() {
+        return 60000;
     }
 
     private double getaDouble(double income, int i, double v, int i2) {
