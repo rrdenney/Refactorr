6063189b7c8af60ea12a672fc7ea76f9935aeb6c
Ryan Denney <rrdenney@bsu.edu>
2019-04-05 11:41:27 -0400

    Extraction

--- src/ifs/Refactoring.java
+++ src/ifs/Refactoring.java
@@ -6,12 +6,16 @@ public class Refactoring {
         if (income <= 10000) {
             return income*0.365;
         } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+            return getaDouble(income, 10000, 0.2, 35600);
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            return getaDouble(income, 30000, 0.1, 76500);
         } else {
-            return (income-60000)*0.02+105600;
+            return getaDouble(income, 60000, 0.02, 105600);
         }
 
+    }
+
+    private double getaDouble(double income, int i, double v, int i2) {
+        return (income - i) * v + i2;
     }
 }
