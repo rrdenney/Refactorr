daf4f1e915a79602380152bfc962f714ae57164e
Ryan Denney <rrdenney@bsu.edu>
2019-04-05 12:57:52 -0400

    Strategy

--- src/ifs/Refactoring.java
+++ src/ifs/Refactoring.java
@@ -2,17 +2,21 @@ package ifs;
 
 public class Refactoring {
 
-    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+    private final InsuranceStrategyVeryHigh strategy;
+
+    {
+        strategy = new InsuranceStrategyVeryHigh();
+    }
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
             return income*0.365;
         } else if (income <= 30000) {
-            return insuranceStrategyVeryHigh.getaDouble(income, 10000, 0.2, 35600);
-        } else if (income <= insuranceStrategyVeryHigh.getAdjustment()) {
-            return insuranceStrategyVeryHigh.getaDouble(income, 30000, 0.1, 76500);
+            return strategy.getaDouble(income, 10000, 0.2, 35600);
+        } else if (income <= strategy.getAdjustment()) {
+            return strategy.getaDouble(income, 30000, 0.1, 76500);
         } else {
-            return insuranceStrategyVeryHigh.getaDouble(income, insuranceStrategyVeryHigh.getAdjustment(), insuranceStrategyVeryHigh.getWeight(), insuranceStrategyVeryHigh.getConstant());
+            return strategy.getaDouble(income, strategy.getAdjustment(), strategy.getWeight(), strategy.getConstant());
         }
 
     }
